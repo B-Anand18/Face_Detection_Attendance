9ace40b5af723f88b1d291df7708c17248239479
B_Anand <anandbiju000@gmail.com>
2024-01-15 12:41:42 +0530

    First Commit

---
+++ AddDataToDatabase.py
@@ -0,0 +1,60 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://faceattendance-fa7be-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+ref = db.reference('Students')
+
+data = {
+
+    "321654":
+        {
+            "name":"Murtaza Hassan",
+            "major":"Robotics",
+            "starting_year":2017,
+            "total_attendance":6,
+            "standing":"6",
+            "year":4,
+            "last_attendance_time":"2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emily Blunt",
+            "major": "Economics",
+            "starting_year": 2018,
+            "total_attendance": 12,
+            "standing": "15",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "AI",
+            "starting_year": 2014,
+            "total_attendance": 1,
+            "standing": "1",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "120116":
+        {
+            "name": "B Anand",
+            "major": "CSE",
+            "starting_year": 2020,
+            "total_attendance": 100,
+            "standing": "999",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+
+
+
+}
+
+for key,value in data.items():
+    ref.child(key).set(value)
---
+++ main.py
@@ -0,0 +1,173 @@
+import os
+import pickle
+import cvzone
+import numpy as np
+import cv2
+import face_recognition
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+from datetime import datetime
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendance-fa7be-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    'storageBucket': "faceattendance-fa7be.appspot.com"
+})
+
+bucket = storage.bucket()
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+# importing the mode images into list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imageModeList = []
+for path in modePathList:
+    imageModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+
+# print(len(imageModeList))
+
+# Load the encoding file
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded ...")
+
+modeType = 0
+counter = 0
+id = -1
+imgStudent = []
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    # imgS = cv2.resize(img, (0, 0), None, 0.1, 0.1)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    faceCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFrame = face_recognition.face_encodings(imgS, faceCurrFrame)
+
+    if not success:
+        print("Failed to capture a frame.")
+        break
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imageModeList[modeType]
+
+    for encodeFace, faceLoc in zip(encodeCurrFrame, faceCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches: ", matches)
+        # print("face distance: ", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        # print("matchIndex: ", matchIndex)
+
+        if matches[matchIndex]:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+            id = studentIds[matchIndex]
+            # print(id)
+            if counter == 0:
+                cvzone.putTextRect(imgBackground,"Loading",(275,400))
+                cv2.imshow("Face Attendance",imgBackground)
+
+                cv2.namedWindow("Face Attendance", cv2.WINDOW_NORMAL)
+                # cv2.resizeWindow("Face Attendance", 800, 600)
+
+                cv2.waitKey(1)
+                counter = 1
+                modeType = 1
+
+        if counter != 0:
+            if counter == 1:
+                # Get the data
+                studentInfo = db.reference(f'Students/{id}').get()
+                print(studentInfo)
+                # Get image from storage
+                blob = bucket.get_blob(f'Images/{id}.png')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgStudent = cv2.imdecode(array, cv2.COLOR_BGR2GRAY)
+
+                # Update data of Attendance
+
+                datetimeObject = datetime.strptime(studentInfo['last_attendance_time'],
+                                                  '%Y-%m-%d %H:%M:%S')
+                secondsElapsed = (datetime.now() - datetimeObject).total_seconds()
+                print(secondsElapsed)
+
+                if secondsElapsed > 30:
+                    ref = db.reference(f'Students/{id}')
+                    studentInfo['total_attendance'] += 1
+                    ref.child('total_attendance').set(studentInfo['total_attendance'])
+                    ref.child('last_attendance_time').set(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                else :
+                    modeType = 3
+                    counter = 0
+                    imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+            if modeType != 3:
+
+                if 10 < counter < 20:
+                    modeType = 2
+
+                imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+
+                if counter <= 10:
+                    cv2.putText(imgBackground, str(studentInfo['total_attendance']), (861, 125),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(id), (1006, 493),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['standing']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+                    (w,h),_ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX,1,1)
+                    offset = (430 - w)//2
+                    cv2.putText(imgBackground, str(studentInfo['name']), (880 + offset - 80, 445),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+                    imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+
+
+                counter += 1
+
+                if counter >= 20:
+                    counter = 0
+                    modeType = 0
+                    studentInfo = []
+                    imgStudent = []
+                    imgBackground[44:44 + 633, 808:808 + 414] = imageModeList[modeType]
+
+
+            # print("Know Face Detected")
+            # print(studentIds[matchIndex])
+
+            # Source shape: (720, 1280, 3)
+            # Destination
+            # region
+            # shape: (545, 371, 3)
+
+    # cv2.imshow("WebCam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
